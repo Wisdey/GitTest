58e71bdef9e5e65a947c3265632dfe236ad61acd
Alessandro Cindolo <alpix@alexs.local>
2017-12-07 10:04:34 +0100

    print

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("It's working")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
